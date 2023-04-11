2c8e8f09ea8adb0267b5dccd5d9f0957350933b9
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-04-11 05:03:56 -0500

    refactor: :recycle: added button to tweet button instead on link

--- src/layouts/SidebarTweetButton.tsx
+++ src/layouts/SidebarTweetButton.tsx
@@ -1,15 +1,19 @@
 import { FeatherIcon } from '@/components'
-import Link from 'next/link'
+import { useStore } from '@/store'
 
 export function SidebarTweetButton() {
+  const openLoginModal = useStore((state) => state.openLoginModal)
+  const handleClick = () => {
+    openLoginModal()
+  }
   return (
-    <Link href='/'>
+    <button onClick={handleClick} className='w-full'>
       <i className='mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-sky-500/80 transition-colors text-white'>
         <FeatherIcon />
       </i>
       <p className='text-center font-semibold text-white text-xl mt-6 hidden lg:block px-4 py-2 rounded-full bg-sky-500 hover:bg-sky-500/80 transition-colors'>
         Tweet
       </p>
-    </Link>
+    </button>
   )
 }
