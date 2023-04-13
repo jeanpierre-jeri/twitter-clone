c860f5d2691f3035aa1609af8fb46672ed3c0064
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-04-13 01:00:28 -0500

    feat: added animations for spinner

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,18 @@ module.exports = {
     './src/layouts/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      animation: {
+        'clip-loader': '0.75s linear 0s infinite normal both running clip-loader'
+      },
+      keyframes: {
+        'clip-loader': {
+          '0%': { transform: 'rotate(0deg) scale(1)' },
+          '50%': { transform: 'rotate(180deg) scale(0.8)' },
+          '100%': { transform: 'rotate(360deg) scale(1)' }
+        }
+      }
+    }
   },
   plugins: []
 }
