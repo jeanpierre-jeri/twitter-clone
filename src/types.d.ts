e9c9f735836ac1624cdc45782885a05247eb6f98
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-04-13 01:01:26 -0500

    feat: add types for User

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,12 @@
+import { type userInfo } from './lib/prisma/users'
+import { type User } from '@prisma/client'
+
 export interface RegisterFormData {
   email: string
   name: string
   username: string
   password: string
 }
+
+export type User = Pick<User, 'id' | 'name' | 'username' | 'bio' | 'email' | 'image' | 'coverImage' | 'profileImage'>
+export type UserWithFollowersCount = User & { followersCount: number }
