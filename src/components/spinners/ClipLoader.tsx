4304749c1ce3ac6f521908fcbe4d321ceaa2e35f
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-04-13 01:01:37 -0500

    feat: add Spinner

---
+++ src/components/spinners/ClipLoader.tsx
@@ -0,0 +1,5 @@
+export function ClipLoader() {
+  return (
+    <span className='w-full aspect-square rounded-full border-2 border-inherit border-r-transparent inline-block animate-clip-loader'></span>
+  )
+}
