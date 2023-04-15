e684a127197bd07f65d327dd46711e1083191ef0
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-04-14 23:24:20 -0500

    feat: add cloudinary

---
+++ src/services/cloudinary.ts
@@ -0,0 +1,14 @@
+import { v2 as cloudinary } from 'cloudinary'
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true
+})
+
+export async function saveImageToCloudinary(image: string) {
+  const { secure_url } = await cloudinary.uploader.upload(image, { upload_preset: 'twitter-clone' })
+
+  return { imageUrl: secure_url }
+}
