279fc07abd40520a751a9bf3edda1571489472ba
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-04-17 19:48:19 -0500

    feat: add message and heart outline icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,4 +1,4 @@
-export function HouseIcon() {
+export function HouseIcon () {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -18,7 +18,7 @@ export function HouseIcon() {
   )
 }
 
-export function BellIcon() {
+export function BellIcon () {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -37,7 +37,7 @@ export function BellIcon() {
   )
 }
 
-export function UserIcon() {
+export function UserIcon () {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -56,7 +56,7 @@ export function UserIcon() {
   )
 }
 
-export function TwitterIcon() {
+export function TwitterIcon () {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -75,7 +75,7 @@ export function TwitterIcon() {
   )
 }
 
-export function LogOutIcon() {
+export function LogOutIcon () {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -95,7 +95,7 @@ export function LogOutIcon() {
   )
 }
 
-export function FeatherIcon() {
+export function FeatherIcon () {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -113,7 +113,7 @@ export function FeatherIcon() {
     </svg>
   )
 }
-export function BackArrowIcon() {
+export function BackArrowIcon () {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -129,7 +129,7 @@ export function BackArrowIcon() {
   )
 }
 
-export function OutlineCloseIcon() {
+export function OutlineCloseIcon () {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -148,7 +148,7 @@ export function OutlineCloseIcon() {
   )
 }
 
-export function CalendarIcon() {
+export function CalendarIcon () {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -168,13 +168,49 @@ export function CalendarIcon() {
   )
 }
 
-export function NoImageIcon() {
+export function NoImageIcon () {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='800' height='800' viewBox='0 0 1920 1920' fill='currentColor'>
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='800'
+      height='800'
+      viewBox='0 0 1920 1920'
+      fill='currentColor'
+    >
       <path
         fillRule='evenodd'
         d='M1920 113v1694.118H0V113h1920Zm-112.941 112.941H112.94V1694.29h1694.12V225.941ZM935.944 1058.77c182.851-258.409 332.498-286.87 426.691-264.847 160.716 37.384 272.076 242.26 330.805 608.98l10.504 65.392H207.924l20.104-71.718c85.384-306.07 172.913-457.072 283.37-489.712 102.55-30.156 185.336 43.82 258.86 109.1 43.596 38.514 90.806 81.092 122.542 78.834 15.925-1.807 32.188-20.556 43.144-36.029Zm401.167-154.842c-83.012-19.313-196.066 60.762-308.895 220.01-35.237 49.807-76.348 77.816-122.315 83.124-76.226 9.024-138.182-42.827-198.812-96.266l-3.868-3.413c-2.578-2.276-5.154-4.552-7.73-6.824-54.211-48.113-110.682-97.017-152.018-85.384-34.447 10.165-102.325 69.12-185.788 340.179h1212.762c-50.485-265.186-134.4-428.386-233.336-451.426ZM677.647 338.882c124.574 0 225.882 101.309 225.882 225.883 0 124.574-101.308 225.882-225.882 225.882S451.765 689.34 451.765 564.765s101.308-225.883 225.882-225.883Zm0 112.942c-62.343 0-112.941 50.71-112.941 112.94 0 62.231 50.598 112.942 112.941 112.942 62.344 0 112.941-50.71 112.941-112.941 0-62.23-50.597-112.941-112.94-112.941Z'
       />
+    </svg>
+  )
+}
+
+export function OutlineMessageIcon () {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      fill="currentColor"
+      stroke="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 1024 1024"
+    ><path stroke="none" d="M464 512a48 48 0 1 0 96 0 48 48 0 1 0-96 0zm200 0a48 48 0 1 0 96 0 48 48 0 1 0-96 0zm-400 0a48 48 0 1 0 96 0 48 48 0 1 0-96 0zm661.2-173.6c-22.6-53.7-55-101.9-96.3-143.3a444.35 444.35 0 0 0-143.3-96.3C630.6 75.7 572.2 64 512 64h-2c-60.6.3-119.3 12.3-174.5 35.9a445.35 445.35 0 0 0-142 96.5c-40.9 41.3-73 89.3-95.2 142.8-23 55.4-34.6 114.3-34.3 174.9A449.4 449.4 0 0 0 112 714v152a46 46 0 0 0 46 46h152.1A449.4 449.4 0 0 0 510 960h2.1c59.9 0 118-11.6 172.7-34.3a444.48 444.48 0 0 0 142.8-95.2c41.3-40.9 73.8-88.7 96.5-142 23.6-55.2 35.6-113.9 35.9-174.5.3-60.9-11.5-120-34.8-175.6zm-151.1 438C704 845.8 611 884 512 884h-1.7c-60.3-.3-120.2-15.3-173.1-43.5l-8.4-4.5H188V695.2l-4.5-8.4C155.3 633.9 140.3 574 140 513.7c-.4-99.7 37.7-193.3 107.6-263.8 69.8-70.5 163.1-109.5 262.8-109.9h1.7c50 0 98.5 9.7 144.2 28.9 44.6 18.7 84.6 45.6 119 80 34.3 34.3 61.3 74.4 80 119 19.4 46.2 29.1 95.2 28.9 145.8-.6 99.6-39.7 192.9-110.1 262.7z" />
+    </svg>
+  )
+}
+
+export function OutlineHeartIcon () {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      fill="currentColor"
+      stroke="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 1024 1024"
+    ><path stroke="none" d="M923 283.6a260.04 260.04 0 0 0-56.9-82.8 264.4 264.4 0 0 0-84-55.5A265.34 265.34 0 0 0 679.7 125c-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5a258.44 258.44 0 0 0-56.9 82.8c-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3.1-35.3-7-69.6-20.9-101.9zM512 814.8S156 586.7 156 385.5C156 283.6 240.3 201 344.3 201c73.1 0 136.5 40.8 167.7 100.4C543.2 241.8 606.6 201 679.7 201c104 0 188.3 82.6 188.3 184.5 0 201.2-356 429.3-356 429.3z" />
     </svg>
   )
 }
