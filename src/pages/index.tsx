5099cbabf378465fe9ab5afde3c130e47ab4c107
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-04-21 23:49:11 -0500

    fix: only show PostFeed if log in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home () {
       </Head>
       <Header label='Home' />
       <Form placeholder="What's happening?" />
-      {!!session && <PostFeed />}
+      {!!session && <PostFeed /> }
     </>
   )
 }
